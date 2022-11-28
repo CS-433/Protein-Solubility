51c2db2f029b3840775ffd296adcfd1bb5dc2319
Matt Dupraz <matthew.dupraz@gmail.com>
2022-11-28 18:01:04 +0100

    Updated One Hot

--- src/data.py
+++ src/data.py
@@ -2,9 +2,9 @@ from pandas import read_csv
 import numpy as np
 
 # Encoding of FASTA letters into integers
-# Starts at 0
+# Starts at 1
 class Encoding:
-    residues = ['A', 'C', 'D', 'E', 'F',
+    residues = ['', 'A', 'C', 'D', 'E', 'F',
         'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P',
         'Q', 'R', 'S', 'T', 'V', 'W', 'Y']
     mapping = dict(zip(residues, range(len(residues))))
@@ -12,18 +12,19 @@ class Encoding:
 # Build a list of integers from string
 # The resulting list is trimmed/padded with 0
 # so that it has length `l`
-def string_to_ints(s, l=1024):
+def string_to_ints(s, l):
     a = [*map(Encoding.mapping.get, s)]
     n = len(a)
     if (n < l):
-        a += [-1]*(l - n)
+        a += [0]*(l - n)
     else:
         a = a[:l]
     return np.array(a)
 
-def load_data(data_path):
+def load_data(data_path, trim_length):
     df = read_csv(data_path)
     y = df.solubility.values
-    x = np.stack(df.fasta.map(lambda st: string_to_ints(st, 400)).values)
+    x = np.stack(df.fasta.map(
+        lambda st: string_to_ints(st, trim_length)).values)
     return (y, x)
 
