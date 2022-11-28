0102544c47369eb1f6ce04ebba23a47767e95632
Matt Dupraz <matthew.dupraz@gmail.com>
2022-11-28 17:53:16 +0100

    Updated One Hot

--- src/data.py
+++ src/data.py
@@ -1,24 +1,29 @@
 from pandas import read_csv
+import numpy as np
 
-residues = ['A', 'C', 'D', 'E', 'F',
- 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P',
- 'Q', 'R', 'S', 'T', 'V', 'W', 'Y']
+# Encoding of FASTA letters into integers
+# Starts at 0
+class Encoding:
+    residues = ['A', 'C', 'D', 'E', 'F',
+        'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P',
+        'Q', 'R', 'S', 'T', 'V', 'W', 'Y']
+    mapping = dict(zip(residues, range(len(residues))))
 
 # Build a list of integers from string
 # The resulting list is trimmed/padded with 0
 # so that it has length `l`
 def string_to_ints(s, l=1024):
-    a = list(map(ord, s))
+    a = [*map(Encoding.mapping.get, s)]
     n = len(a)
     if (n < l):
-        a += [0]*(l - n)
+        a += [-1]*(l - n)
     else:
         a = a[:l]
-    return torch.tensor(a)
+    return np.array(a)
 
 def load_data(data_path):
     df = read_csv(data_path)
     y = df.solubility.values
-    x = np.stack(df.fasta.map(lambda st: transform(st, 400)).values)
+    x = np.stack(df.fasta.map(lambda st: string_to_ints(st, 400)).values)
     return (y, x)
 
