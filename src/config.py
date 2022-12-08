750d176a58ade0d57d80bab7f1c55ac8d2d0161b
Lubor Budaj <luboroo@gmail.com>
2022-12-08 22:08:32 +0100

    rnn model

--- src/config.py
+++ src/config.py
@@ -1,9 +1,9 @@
 config = {
     "max_chain_length": 600,
-    "num_epochs": 500,
+    "num_epochs": 1000,
     "eval_step": 10,
-    "batch_size": 32,
-    "weight_decay": 5e-3,
+    "batch_size": 256,
+    "weight_decay": 5e-1,
     "learning_rate": 1e-3
 }
 
@@ -14,10 +14,11 @@ model_config = {
         [4, 3, 5, 1, 2],
         [3, 2, 5, 1, 2]
     ],
+    "rnn": [600, 300, 3],
     "clf": [
         # in_features, #out_features, bias
         [config["max_chain_length"]*2, config["max_chain_length"], True],
         [config["max_chain_length"], 1, True],
     ],
-    "dropout_p": 0.2
+    "dropout_p": 0.3
 }
--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import torch.nn as nn
 
-from config import config, model_config
+from config import model_config
 
 
 class PrintLayer(nn.Module):
@@ -17,3 +17,15 @@ model = nn.Sequential(
     nn.Flatten(),
     *sum([[nn.Linear(*sizes), nn.BatchNorm1d(sizes[1]), nn.Dropout(p=model_config["dropout_p"])] for sizes in model_config["clf"]], [])
 )
+
+class model2(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.rnn = nn.GRU(*model_config["rnn"], batch_first=True)           #We can use LSTM/GRU/RNN
+        self.seq = nn.Sequential(nn.Dropout(p=model_config["dropout_p"]), nn.SiLU(), nn.Flatten(),
+                                 nn.Linear(model_config["rnn"][1] * 20, 1), nn.BatchNorm1d(1), nn.Dropout(p=model_config["dropout_p"]))
+
+    def forward(self, x):
+        x = self.rnn(x)
+        x = self.seq(x[0])
+        return x
